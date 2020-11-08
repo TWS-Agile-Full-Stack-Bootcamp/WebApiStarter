025cccfcec6f535faedfaf283f67945f591529b4
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-08 22:08:56 +0800

    test: add controller unit test

---
+++ WebApiStarterTest/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using WebApiStarter.Controllers;
+using Xunit;
+
+namespace WebApiStarterTest
+{
+    public class HelloControllerTest
+    {
+        [Fact]
+        public void Should_return_hello_world_with_default_request()
+        {
+            //given
+            var orderController = new HelloController();
+
+            //when
+            ObjectResult actionResult = orderController.Get() as ObjectResult;
+
+            //then
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)actionResult.StatusCode);
+            Assert.Equal("Hello World", actionResult.Value);
+        }
+    }
+}
